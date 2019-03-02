3110206d678905d3323dce8489db63dc7b8f7cf7
vladimir86ns <vladimir86ns@gmail.com>
2019-03-02 19:59:14 +0100

    feature - add blog page and contact us page

---
+++ src/app/pages/contact-us-page/contact-us-page.component.ts
@@ -0,0 +1,33 @@
+import { SharedAnimations } from 'src/app/shared/animations/shared-animations';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact-us-page',
+  templateUrl: './contact-us-page.component.html',
+  styleUrls: ['./contact-us-page.component.css'],
+  animations: [SharedAnimations]
+})
+export class ContactUsPageComponent implements OnInit {
+  private formValues = {
+    'email': null,
+    'name': null,
+    'subject': null,
+    'message': null,
+
+  };
+ staticAlertClosed : true;
+  error = null;
+  success: any = null;
+  constructor() {}
+
+  ngOnInit() {}
+
+  submitForm(form: NgForm) {
+    this.formValues.email = form.value.email;
+    this.formValues.name = form.value.name;
+    this.formValues.subject = form.value.subject;
+    this.formValues.message = form.value.message;
+    this.success=true;
+  }
+}
